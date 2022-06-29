2b479fb1b87df87a3edfb2ba242184a6c12d9d9b
TomasLx <langenekertomas@gmail.com>
2022-06-29 15:47:17 -0300

    Fase2

--- blog/blogcoder/urls.py
+++ blog/blogcoder/urls.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import inicio
+from .views import inicio, login, registro
+from django.contrib.auth.views import LogoutView
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('login/', login, name='login'),
     path("inicio/", inicio, name="inicio" ),
+    path("registro/", registro, name="registro"),
+    path("logout/", LogoutView.as_view(template_name='blogcoder/logout.html'), name="logout"),
     ]

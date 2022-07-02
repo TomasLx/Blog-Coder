6f5cfc89f6fde6fc0488df488228bcb9890a598e
TomasLx <langenekertomas@gmail.com>
2022-07-02 18:42:06 -0300

    Fase3

--- blog/blogcoder/urls.py
+++ blog/blogcoder/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import inicio, login_request, registro, blog, BlogCreacion, BlogDetalle
+from .views import inicio, login_request, registro, blog, BlogCreacion, BlogDetalle, BlogDelete, BlogUpdate, editarPerfil
 from django.contrib.auth.views import LogoutView
 
 urlpatterns = [
@@ -9,7 +9,9 @@ urlpatterns = [
     path("registro/", registro, name="registro"),
     path("logout/", LogoutView.as_view(template_name='blogcoder/logout.html'), name="logout"),
     path("blog/", blog, name="blog"),
-    path("blogcreacion", BlogCreacion.as_view(), name='New'),
+    path("blogcreacion/", BlogCreacion.as_view(), name='New'),
+    path("blogdetalle/<pk>", BlogDelete.as_view(), name='Delete'),
+    path("blogupdate/<pk>", BlogUpdate.as_view(), name='Update'),
     path("blog/<pk>", BlogDetalle.as_view(), name='Detalle'),
-
+    path("editarperfil/", editarPerfil, name='Editar Perfil'),
     ]
